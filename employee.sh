e8dcc1558dac8a5f0fbfeb4380618055a2e44b2c
Sunit47 <Sunitajgar47@gmail.com>
2022-08-04 14:05:18 +0530

    Checks whether employee is present or absent.

--- employee.sh
+++ employee.sh
@@ -2,3 +2,13 @@
 echo ""
 echo "Welcome to Employee Wage Computation Program"
 echo ""
+
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent -eq $randomCheck ];
+then
+    echo "Employee is present";
+else
+    echo "Employee is Absent";
+fi
